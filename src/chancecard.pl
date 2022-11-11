7541aa9fb0468d36db640db8dae145373d3c47c8
Gulilil <juan.csantoso@gmail.com>
2022-11-12 02:42:50 +0700

    adding new func in chance card

--- src/chancecard.pl
+++ src/chancecard.pl
@@ -1,10 +1,14 @@
 /* List kartu pada game tersebut 
     Jenis Kartu                     | Probabilitas  | Deskripsi
     - Kartu Tax                     |       20%     | Pemain akan ditempatkan ke tempat tax terdekat 
-    - Kartu Hadiah                  |       20%     | Pemain mendapatkan uang dengan nominal tertentu
-    - Kartu Get out from Jail       |       20%     | Pemain mendapatkan kesempatan untuk tidak masuk penjara
+    - Kartu Hadiah                  |               | Pemain mendapatkan uang dengan nominal tertentu
+            S 50                    |       10%     |
+            S 100                   |       7,5%    |
+            S 200                   |       2,5%    |
+    - Kartu Get out from Jail       |       15%     | Pemain mendapatkan kesempatan untuk tidak masuk penjara
     - Kartu Go to Jail              |       20%     | Pemain langsung dimasukkan ke penjara
     - Kartu Go to World Tour        |       15%     | Pemain langsung ditempatkan kepada petak World Tour
+    - Kartu Steal Property          |       5%      | Pemain dapat mengakuisisi properti pemain lain (kecuali LandMark).
     - Kartu Angel Card              |      4,9%     | Bila pemain berada di situasi menuju bangkrut, pemain dapat menggunakan kartu ini untuk selamat
     - Kartu Auto Win                |      0,1%     | Pemain langsung memenangkan permainan, tetapi persentase mendapatkan kartu ini sangat kecil
 */
@@ -12,30 +16,81 @@
 /* Variabel untuk melihat bahwa pemain sedang berada pada petak CC */
 :- dynamic(inCC/1). 
 
+/* Fakta Kartu */
+card('Go To Tax').
+card('Free $50').
+card('Free $100').
+card('Free $200').
+card('Get Out From Jail').
+card('Go To Jail').
+card('Go To World Tour').
+card('Steal Property').
+card('Angel Card').
+card('Instant Win').
+
+indexCard('Instant Win', 1).
+indexCard('Angel Card', 2).
+indexCard('Go To Tax', 3).
+indexCard('Free $50', 4).
+indexCard('Free $100', 5).
+indexCard('Free $200', 6).
+indexCard('Get Out From Jail', 7).
+indexCard('Go To Jail', 8).
+indexCard('Go To World Tour', 9).
+indexCard('Steal Property', 10).
+
 
 /* Randomizer untuk chance card 
     Menggunakan metode LCG
     X[n+1] = (X[n]*(a) + c) mod m
     a, c, m adalah -> a = 77, c = Money+537, m = 1000
-    Nilai Seed atau X0 adalah sebesar (1234 + Money)
+    Nilai Seed atau X0 adalah sebesar (1234 + Money) 
     Money adalah banyaknya uang player saat pengambilan chance card
     Banyaknya iterasi yang dilakukan untuk metode LCG adalah nilai round pada pelaksanaan chance card
 */
 
+
+
+/* Menentukan Random Integer */
 /* Fakta */
 calculateRandomChance(Money, 0, 1234+Money).
-/* Bila nilai iterasi sama dengan round, maka rekursif telah selesai dijalankan */
 
 /* Rules */
-calculateRandomChance(Money, Round, Result) :- 
-                    (calculateRandomChance(Money, NewRound, NewResult), Result is (((NewResult*(Money+56)+Money+537) mod 1000)), Round is NewRound + 1).
+calculateRandomChance(Money, Round, Result) :- calculateRandomChance(Money, NewRound, NewResult), Round is NewRound + 1 , Result is (((NewResult*(Money+56)+Money+537) mod 1000)), !.
+
+
+/* Menentukan Kartu yang Didapat */
+/* Fakta */
+
+/* Rules */
+getChanceCard(Money, Round, Result) :- calculateRandomChance(Money, Round, RandInt), 
+                                        (RandInt = 777, Result is indexCard(Result, 1); 
+                                        RandInt >= 750, RandInt =< 799, Result is indexCard(Result, 2);
+                                        RandInt >= 700, RandInt =< 749, Result is indexCard(Result, 10);
+                                        RandInt >= 0, RandInt =< 199, Result is indexCard(Result, 3);
+                                        RandInt >= 200, RandInt =< 299, Result is indexCard(Result, 4);
+                                        RandInt >= 300, RandInt =< 374, Result is indexCard(Result, 5);
+                                        RandInt >= 375, RandInt =< 399, Result is indexCard(Result, 6);
+                                        RandInt >= 400, RandInt =< 549, Result is indexCard(Result, 7);
+                                        RandInt >= 550, RandInt =< 699, Result is indexCard(Result, 9);
+                                        RandInt >= 800, RandInt =< 999, Result is indexCard(Result, 8)).
+
 
 
 
 /* 
 Untuk menyimpan sementara
 
+calculateRandomChance(_,_,1234,1).
+/* Pada iterasi ke 0, result yang dikeluarkan adalah sesuai dengan money 
+
+calculateRandomChance(Money, Round, _, Iteration) :-
+    (Iteration = 1,  NewIteration is Iteration+1, calculateRandomChance(Money, Round, 1234+Money, NewIteration)).
+    /* Dilakukan untuk iterasi pertama 
+
 # If Else
 kondisi -> hasil true ; hasil false 
+
+Result is (((NewResult*(Money+56)+Money+537) mod 1000))
 
 */
