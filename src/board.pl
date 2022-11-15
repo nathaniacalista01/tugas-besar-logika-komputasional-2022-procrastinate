b215541762bf16077dc23f1c0dff2d4df42580cf
nathania01 <nathania.calista01@gmail.com>
2022-11-15 16:34:01 +0700

    feat : add update location

--- src/board.pl
+++ src/board.pl
@@ -3,40 +3,41 @@
 /* kepemilikan properti dan posisi pemain belum diimplementasikan */
 :- include('pemain.pl').
 
-player(_,Loc,_,_,_,_) :- write(Loc).
+tes :- 
+    tile(_,__,Posisi,8),write(Posisi).
 
-tile(1,2,'FP').
-tile(1,3,'D3').
-tile(1,4,'D2').
-tile(1,5,'D1').
-tile(1,6,'TX').
-tile(1,7,'C3').
-tile(1,8,'C2').
-tile(1,9,'C1').
-tile(1,10,'JL').
-tile(2,10,'B3').
-tile(3,10,'B2').
-tile(4,10,'B1').
-tile(5,10,'CC').
-tile(6,10,'A3').
-tile(7,10,'A2').
-tile(8,10,'A1').
-tile(9,10,'GO').
-tile(2,2,'E1').
-tile(3,2,'E2').
-tile(4,2,'E3').
-tile(5,2,'CC').
-tile(6,2,'F1').
-tile(7,2,'F2').
-tile(8,2,'F3').
-tile(9,2,'WT').
-tile(9,3,'G1').
-tile(9,4,'G2').
-tile(9,5,'G3').
-tile(9,6,'TX').
-tile(9,7,'CC').
-tile(9,8,'H1').
-tile(9,9,'H2').
+tile(1,2,'FP',16).
+tile(1,3,'D3',15).
+tile(1,4,'D2',14).
+tile(1,5,'D1',13).
+tile(1,6,'TX',12).
+tile(1,7,'C3',11).
+tile(1,8,'C2',10).
+tile(1,9,'C1',9).
+tile(1,10,'JL',8).
+tile(2,10,'B3',7).
+tile(3,10,'B2',6).
+tile(4,10,'B1',5).
+tile(5,10,'CC',4).
+tile(6,10,'A3',3).
+tile(7,10,'A2',2).
+tile(8,10,'A1',1).
+tile(9,10,'GO',0).
+tile(2,2,'E1',17).
+tile(3,2,'E2',18).
+tile(4,2,'E3',19).
+tile(5,2,'CC',20).
+tile(6,2,'F1',21).
+tile(7,2,'F2',22).
+tile(8,2,'F3',23).
+tile(9,2,'WT',24).
+tile(9,3,'G1',25).
+tile(9,4,'G2',26).
+tile(9,5,'G3',27).
+tile(9,6,'TX',28).
+tile(9,7,'CC',29).
+tile(9,8,'H1',30).
+tile(9,9,'H2',31).
 
 boardSize(9,9).
 
@@ -64,26 +65,26 @@ drawTile(9,12) :- nl.
 drawTile(1,Y) :- boardSize(_,H),
                     Y > 1,
                     Y < H + 2, !,
-                    tile(1,Y,Obj), !,
+                    tile(1,Y,Obj,_), !,
                     write('| '), write(Obj), write(' |'),
                     drawTile(2,Y).
 drawTile(9,Y) :- boardSize(_,H),
                     Y > 2,
                     Y < H + 1, !,
-                    tile(9,Y,Obj), !,
+                    tile(9,Y,Obj,_), !,
                     write(' '), write(Obj), write(' |'),
                     drawTile(10,Y).
 drawTile(X,2) :- boardSize(W,_),
                     X > 1,
                     X < W + 1,
-                    tile(X,2,Obj), !,
+                    tile(X,2,Obj,_), !,
                     write(' '), write(Obj), write(' |'),
                     X1 is X + 1,
                     drawTile(X1,2).
 drawTile(X,10) :- boardSize(W,_),
                     X > 1,
                     X < W + 1,
-                    tile(X,10,Obj), !,
+                    tile(X,10,Obj,_), !,
                     write(' '), write(Obj), write(' |'),
                     X1 is X + 1,
                     drawTile(X1,10).
