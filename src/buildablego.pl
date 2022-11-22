0ccf57107271d78970773acaf732e2717b579e72
energizedNachos <16521108@mahasiswa.itb.ac.id>
2022-11-22 16:29:58 +0700

    feat: add algoritma dasar buildable go

---
+++ src/buildablego.pl
@@ -0,0 +1,37 @@
+include('pemain.pl').
+include('board.pl').
+include('location.pl').
+
+:- dynamic(buildAccess/1).
+
+goBuildProperti() :-
+    checkInGo(), askIfWantToBuild().
+
+/* CheckInGo selalu dijalankan setelah melempar dadu */
+checkInGo() :-
+    /* Gain Access Build Go*/
+    player1(Id,Loc,Money,Property,List1,List2),
+    tile(_,_,Loc,LocNumber),
+    /* Cek Lokasi Player apakah ada di Go */
+    LocNumber =:= 1,
+    /* Memberikan player access untuk build properti */
+    asserta(buildAccess(1)),!.
+
+/* Tanyakan apakah ingin membangun properti */
+askIfWantToBuild() :-
+    /* Cek apakah player berada di Go */
+    buildAccess(1),
+    write('Apakah anda ingin membangun properti? (y/n)'),nl,
+    read(Answer),
+    /* Jika ya, maka tampilkan list properti yang bisa dibangun */
+    /* Tampilkan list properti yang bisa dibangun */
+    (Answer == 'yes' -> (
+        write('List properti yang bisa dibangun : '),nl,
+        afterBuild()
+    );
+
+    ((Answer == 'no' -> (); 
+        write('Input tidak valid >:( !, jawab hanya (yes/no)',nl)))),!.
+
+afterBuild() :-
+    retract(buildAccess(_)),!.
