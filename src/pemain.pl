a037a5fb85ccb1d152e5960a1641b7f1f7ddde05
Bitha17 <16521076@mahasiswa.itb.ac.id>
2022-11-28 08:48:41 +0700

    feat: sell property N

--- src/pemain.pl
+++ src/pemain.pl
@@ -97,3 +97,13 @@ checkPlayerLocationByID(ID, Loc) :-
 
 writeCard(_,[]).
 writeCard(ID,[H|T]) :- write(ID),write('. '),write(H),nl,NewId is ID + 1,writeCard(NewId,T),!.
+
+resetPropertyN(31,ID,NO,N) :- tile(_,_,Loc,31), locOwnerDetail(Loc, IDPlayer, PropertyLevel), NO == N, !,
+                           (IDPlayer == ID -> retract(locOwnerDetail(Loc,IDPlayer, PropertyLevel)), asserta(locOwnerDetail(Loc,'-','-'))).
+resetPropertyN(IDX,ID,NO,N) :- tile(_,_,Loc,IDX), locOwnerDetail(Loc, IDPlayer, PropertyLevel), NO == N, ID == IDPlayer,
+                           (IDPlayer == ID -> retract(locOwnerDetail(Loc, IDPlayer, PropertyLevel)), asserta(locOwnerDetail(Loc,'-','-'))).
+resetPropertyN(IDX,ID,NO,N) :- tile(_,_,Loc,IDX), locOwnerDetail(Loc, ID, PropertyLevel), NO =< N,
+                            IDX1 is IDX + 1, NO1 is NO + 1, resetPropertyN(IDX1,ID,NO1,N).                            
+resetPropertyN(IDX,ID,NO,N) :- IDX1 is IDX + 1, resetPropertyN(IDX1,ID,NO,N).
+
+sellPropertyN(ID,N) :- resetPropertyN(1,ID,1,N),!.
